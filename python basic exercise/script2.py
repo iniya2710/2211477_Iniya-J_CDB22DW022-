adb49ae8eaa2555460db462281c09581db3e2dcc
Iniya <95535309+iniya2710@users.noreply.github.com>
2022-09-14 10:21:15 +0530

    Add files via upload

---
+++ python basic exercise/script2.py
@@ -0,0 +1,11 @@
+num1 = int(input('Enter the lower limit:'))
+num2 = int(input('Enter the upper limit:'))
+
+ 
+for i in range (num1, num2 + 1):  
+    if i > 1:  
+        for j in range (2, i):  
+            if (i % j) == 0:  
+                break  
+        else:  
+            print (i) 
